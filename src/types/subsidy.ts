d3a0490faf88e81d42f4b13b180750c3014046c6
onikun94 <onikun94@gmail.com>
2022-04-29 05:07:08 +0900

    subsidy type

---
+++ src/types/subsidy.ts
@@ -0,0 +1,10 @@
+export type SubsidyType = {
+  acceptance_end_datetime: string;
+  acceptance_start_datetime: string;
+  id: string;
+  name: string;
+  subsidy_max_limit: number;
+  target_area_search: string;
+  target_number_of_employees: string;
+  title: string;
+};
