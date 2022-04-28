5417500999e0b7da2881a19a999d377352cd0a11
onikun94 <onikun94@gmail.com>
2022-04-29 07:22:44 +0900

    change layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode, VFC } from 'react';
-import Footer from './orgamisms/Footer';
+import Footer from './atoms/Footer';
+import { Header } from './atoms/Header';
 
 type LayoutPropsType = {
   children?: ReactNode;
@@ -8,6 +9,7 @@ type LayoutPropsType = {
 const Layout: VFC<LayoutPropsType> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen  px-12 pt-6 mx-auto w-max  h-auto ">
+      <Header src="/expact1.png" width="192" height="50" />
       <main className="flex-grow">{children}</main>
       <Footer />
     </div>
