881b3bce5901b7dc7a99e60b6988d996daba3f59
onikun94 <onikun94@gmail.com>
2022-04-29 07:37:15 +0900

    select type

--- src/components/atoms/Card.tsx
+++ src/components/atoms/Card.tsx
@@ -1,9 +1,18 @@
 import React, { ReactNode, VFC } from 'react';
+
+const bgColorList = ['bg-white', 'bg-gray-50', 'bg-red-50'] as const;
+const roundList = ['rounded-sm', 'rounded-md', 'rounded-xl'] as const;
+
+export type bgColorType = typeof bgColorList[number];
+export type roundType = typeof roundList[number];
+
 type CardPropsType = {
+  round?: roundType;
+  bgColor?: bgColorType;
   children?: ReactNode;
 };
-const Card: VFC<CardPropsType> = ({ children }) => {
-  return <div className="  rounded-xl shadow bg-white   ">{children}</div>;
+const Card: VFC<CardPropsType> = ({ round = 'rounded-sm', bgColor = 'bg-white', children }) => {
+  return <div className={round + ' ' + bgColor + ' ' + 'shadow'}>{children}</div>;
 };
 
 export default Card;
