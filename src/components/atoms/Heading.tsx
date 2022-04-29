a6b537b745dca481b57c9f3b9df40a74e66b8946
onikun94 <onikun94@gmail.com>
2022-04-29 13:16:04 +0900

    select m-y

--- src/components/atoms/Heading.tsx
+++ src/components/atoms/Heading.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 const textSizeList = ['text-xs', 'text-sm', 'text-md', 'text-xl', 'text-2xl', 'text-3xl'] as const;
-export type TextType = typeof textSizeList[number];
+const marginYList = ['my-1', 'my-2', 'my-4', 'my-6', 'my-8'];
+export type textType = typeof textSizeList[number];
+export type marginYType = typeof marginYList[number];
 
 type HeadingPropsType = {
   heading: string;
-  textSize: TextType;
+  textSize: textType;
+  marginY?: marginYType;
 };
 
-export const Heading: React.VFC<HeadingPropsType> = ({ heading, textSize }) => {
-  return <p className={textSize + ' ' + 'm-4'}>{heading}</p>;
+export const Heading: React.VFC<HeadingPropsType> = ({ heading, textSize, marginY = 'my-4' }) => {
+  return <p className={textSize + ' ' + 'mx-2' + ' ' + marginY + ' '}>{heading}</p>;
 };
