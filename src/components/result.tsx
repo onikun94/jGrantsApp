6da196201ce59d99f9b1abddf716990e6468f759
onikun94 <onikun94@gmail.com>
2022-05-09 19:22:37 +0900

    change style

--- src/components/result.tsx
+++ src/components/result.tsx
@@ -1,15 +1,19 @@
 import Link from 'next/link';
 import React from 'react';
+import { SubsidyType } from '../types/subsidy';
 import Card from './atoms/Card';
 
-const Result = ({ res }) => {
+type resultPropsType = {
+  res: SubsidyType[];
+};
+const Result: React.VFC<resultPropsType> = ({ res }) => {
   console.log('RESULT = ', res);
 
   return (
     <>
       {res.map((result) => (
-        <Card>
-          <div key={result.id} className="m-8 p-8 ">
+        <Card round="rounded-xl" bgColor="bg-white">
+          <div key={result.id} className=" mb-8 p-8 ">
             <h2 className="mx-4 pb-1 border-b-2">
               {result.title.length > 30 ? `${result.title.substring(0, 40)}...` : result.title}
             </h2>
@@ -18,9 +22,6 @@ const Result = ({ res }) => {
               {result.acceptance_end_datetime.substring(0, 10)}
             </p>
             <p className="mx-4">補助金額上限：{result.subsidy_max_limit}円</p>
-            {/* <button onClick={() => Router.push({ query: { id: result.id } })}>
-            詳細
-          </button> */}
             <div className="mx-4 mt-5 ">
               <Link href={`contents/${result.id}`}>
                 <a className=" py-2 px-4 text-center font-medium rounded-md text-indigo-700 bg-transparent border border-indigo-700 cursor-pointer">
