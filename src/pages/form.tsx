4a6532ac30c54453af34f7803e4a56ebfe0ae2ca
onikun94 <onikun94@gmail.com>
2022-05-13 13:55:38 +0900

    Refactoring form base

--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -2,17 +2,21 @@ import emailjs from '@emailjs/browser';
 import Link from 'next/link';
 import React, { useRef, useState } from 'react';
 import { useRecoilValue } from 'recoil';
-import Card from '../components/atoms/Card';
 import Layout from '../components/Layout';
+import { FormCard } from '../components/templates/FormCard';
 import { subsidyState } from '../recoil/atoms/subsidy';
 type FormPropsType = {
   subject: string;
 };
 
 const Form = () => {
-  const serviceID = process.env.REACT_APP_SERVICE_ID;
-  const templateID = process.env.REACT_APP_TEMPLATE_ID;
-  const publicKey = process.env.REACT_APP_PUBLIC_KEY;
+  // const serviceID = process.env.NEXT_PUBLIC_SERVICE_ID;
+  // const templateID = process.env.NEXT_PUBLIC_TEMPLATE_ID;
+  // const publicKey = process.env.NEXT__PUBLIC_KEY;
+  // console.log('serviceID = ', serviceID);
+  const serviceID = 'onikun94';
+  const templateID = 'jgrants';
+  const publicKey = '6M_f2dfHKPBVoj7IA';
 
   const form = useRef();
   const getTitle = useRecoilValue(subsidyState);
@@ -23,10 +27,10 @@ const Form = () => {
 
     emailjs.sendForm(serviceID, templateID, form.current, publicKey).then(
       (result) => {
-        console.log(result.text);
+        console.log('result = ', result.text);
       },
       (error) => {
-        console.log(error.text);
+        console.log('error = ', error.text);
       },
     );
   };
@@ -34,6 +38,17 @@ const Form = () => {
   const handleChange = (event) => {
     setTitleValue(event.target.value);
   };
+  const formData = [
+    { formType: 'text', formName: '名前', inputName: 'user_name' },
+    { formType: 'email', formName: 'Email', inputName: 'user_email' },
+    {
+      formType: 'text',
+      formName: '件名',
+      inputName: 'subject',
+      formChange: handleChange,
+      formValue: titleValue,
+    },
+  ];
 
   return (
     <Layout>
@@ -43,33 +58,13 @@ const Form = () => {
         </a>
       </Link>
       <h1 className="m-2">お問い合わせ</h1>
-      <Card>
-        <form ref={form} onSubmit={sendEmail} className="m-2 p-4">
-          <label>
-            名前
-            <input type="text" name="user_name" />
-          </label>
-          <br />
-          <label>
-            Email
-            <input type="email" name="user_email" />
-          </label>
-          <br />
-          <label>
-            件名
-            <input type="text" name="subject" value={titleValue} onChange={handleChange} />
-          </label>
-          <br />
-          <label>
-            内容
-            <textarea className="border" name="message" />
-          </label>
-          <br />
-          <button className="m-2" type="submit" value="Send">
-            提出
-          </button>
-        </form>
-      </Card>
+      <FormCard
+        refForm={form}
+        submit={sendEmail}
+        forms={formData}
+        bgColor="bg-white"
+        rList="rounded-md"
+      />
     </Layout>
   );
 };
