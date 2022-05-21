48018c08ca314f47334cdf50719d1dcd5cf4c68a
onikun94 <onikun94@gmail.com>
2022-05-21 18:28:21 +0900

    for export

--- src/components/molecules/BorderHeading.tsx
+++ src/components/molecules/BorderHeading.tsx
@@ -4,7 +4,7 @@ import { Heading, HeadingPropsType } from '../atoms/Heading';
 const borderWeight = ['border-l-2', 'border-l-4', 'border-l-8'] as const;
 const borderColor = ['border-l-gray-500', 'border-l-gray-600', 'border-l-blue-500'] as const;
 type borderWeightType = typeof borderWeight[number];
-type borderColorType = typeof borderColor[number];
+export type borderColorType = typeof borderColor[number];
 interface BorderHeadingPropsType extends HeadingPropsType {
   borderWeight: borderWeightType;
   borderColor: borderColorType;
