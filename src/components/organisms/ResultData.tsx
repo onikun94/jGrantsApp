4cb08d1cee0b36ab1f59700b5134a574a355afe2
onikun94 <onikun94@gmail.com>
2022-05-21 18:25:59 +0900

    add resultData

---
+++ src/components/organisms/ResultData.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { ButtonLink } from '../atoms/ButtonLink';
+import { Heading } from '../atoms/Heading';
+
+type ResultDataPropsType = {
+  title: string;
+  period: string;
+  money: string;
+  buttonLink: string;
+  buttonName: string;
+};
+
+export const ResultData: React.VFC<ResultDataPropsType> = ({
+  title,
+  period,
+  money,
+  buttonLink,
+  buttonName,
+}) => {
+  return (
+    <div className=" mb-8 p-8 ">
+      <Heading
+        heading={title}
+        textSize="text-xl"
+        marginX="mx-4"
+        border="border-b-2"
+        custom="pb-1"
+      />
+      <Heading heading={period} textSize="text-md" marginX="mx-4" />
+      <Heading heading={money} textSize="text-md" marginX="mx-4" marginY="my-2" />
+      <ButtonLink hrefLink={buttonLink} linkName={buttonName} />
+    </div>
+  );
+};
