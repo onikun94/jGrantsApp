e5068492334ce90509530f01309febddf576ca5f
onikun94 <onikun94@gmail.com>
2022-05-25 15:03:20 +0900

    add router

--- src/lib/handler.ts
+++ src/lib/handler.ts
@@ -1,4 +1,20 @@
+import Router from 'next/router';
+
 export const handleInsertWord = (name: string) => {
   let innerText = document.getElementById('inputText') as HTMLInputElement;
   innerText.value = name;
+};
+
+export const doHandleSubmit = (data) => {
+  Router.push({
+    query: {
+      keyword: data.keyword,
+      sort: data.sort,
+      order: data.order,
+      acceptance: data.restriction,
+      prefecture: data.prefecture,
+      industry: data.industry,
+      employ: data.employ,
+    },
+  });
 };
