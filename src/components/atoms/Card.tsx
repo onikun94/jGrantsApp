8a48e326912048ec9cfbf3a4f8a61b87b50e0668
onikun94 <onikun94@gmail.com>
2022-05-25 16:00:19 +0900

    add change

--- src/components/atoms/Card.tsx
+++ src/components/atoms/Card.tsx
@@ -9,10 +9,15 @@ export type roundType = typeof roundList[number];
 type CardPropsType = {
   round?: roundType;
   bgColor?: bgColorType;
+  handleChange?: () => void;
   children?: ReactNode;
 };
-const Card: VFC<CardPropsType> = ({ round = 'rounded-sm', bgColor, children }) => {
-  return <div className={round + ' ' + bgColor + ' ' + 'shadow'}>{children}</div>;
+const Card: VFC<CardPropsType> = ({ round = 'rounded-sm', bgColor, handleChange, children }) => {
+  return (
+    <div className={round + ' ' + bgColor + ' ' + 'shadow'} onClick={handleChange}>
+      {children}
+    </div>
+  );
 };
 
 export default Card;
