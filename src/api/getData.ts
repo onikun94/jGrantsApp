2f9c45b78ef1da8928f182c85fc08ba8fb90d961
onikun94 <onikun94@gmail.com>
2022-07-02 22:20:12 +0900

    change get detail

--- src/api/getData.ts
+++ src/api/getData.ts
@@ -1,11 +1,24 @@
 export const getData = async (url: string) => {
   console.log(typeof url);
-  console.log("thisurl = ", url);
+  console.log('thisurl = ', url);
   const res = await fetch(url, {
-    method: "GET",
-    headers: { accept: "application/json" },
+    method: 'GET',
+    headers: { accept: 'application/json' },
   });
   const _res = await res.json();
 
   return _res;
+};
+
+export const getDataDetail = async (url: string) => {
+  console.log(typeof url);
+  console.log('thisurl = ', url);
+  const res = await fetch(url, {
+    method: 'GET',
+    headers: { accept: 'application/json' },
+  });
+  const _res = await res.json();
+  console.log('getDataDetail = ', _res);
+
+  return _res.result;
 };
