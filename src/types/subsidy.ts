fcb88e88614cf4724ec8c8acfc8c9a60a57cebb2
onikun94 <onikun94@gmail.com>
2022-07-02 22:18:37 +0900

    detail type

--- src/types/subsidy.ts
+++ src/types/subsidy.ts
@@ -8,3 +8,31 @@ export type SubsidyType = {
   target_number_of_employees: string;
   title: string;
 };
+
+export type SubsidyDetailType = {
+  acceptance_end_datetime: string;
+  acceptance_start_datetime: string;
+  application_form: [{ name: string; data: string }];
+  application_guidelines: [{ name: string; data: string }];
+  detail: string;
+  front_subsidy_detail_page_url: string;
+  id: string;
+  industry: string;
+  is_enable_multiple_request: string;
+  name: string;
+  outline_of_grant: [name: string, data: string];
+  project_end_deadline: string;
+  request_reception_presence: string;
+  subsidy_catch_phrase: string;
+  subsidy_max_limit: number;
+  subsidy_rate: string;
+  target_area_detail: string;
+  target_number_of_employees: string;
+  title: { title: string } | ((currVal: { title: string }) => { title: string });
+  use_purpose: string;
+};
+
+export type AllSubsidyDetailType = {
+  metadata: { resultset: { count: number }; type: string };
+  result: SubsidyDetailType[];
+};
